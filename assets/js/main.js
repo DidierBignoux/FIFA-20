88235385a99f5ab1243a5748decb472415744282
Bignoux Didier <didieransleybignoux@live.com>
2021-07-28 19:20:09 +0400

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,102 @@
+/*
+	Dopetrope by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function ($) {
+  var $window = $(window),
+    $body = $("body");
+
+  // Breakpoints.
+  breakpoints({
+    xlarge: ["1281px", "1680px"],
+    large: ["981px", "1280px"],
+    medium: ["737px", "980px"],
+    small: [null, "736px"],
+  });
+
+  // Play initial animations on page load.
+  $window.on("load", function () {
+    window.setTimeout(function () {
+      $body.removeClass("is-preload");
+    }, 100);
+  });
+
+  // Dropdowns.
+  $("#nav > ul").dropotron({
+    mode: "fade",
+    noOpenerFade: true,
+    alignment: "center",
+  });
+
+  // Nav.
+
+  // Title Bar.
+  $('<div id="titleBar">' + '<a href="#navPanel" class="toggle"></a>' + "</div>").appendTo($body);
+
+  // Panel.
+  $('<div id="navPanel">' + "<nav>" + $("#nav").navList() + "</nav>" + "</div>")
+    .appendTo($body)
+    .panel({
+      delay: 500,
+      hideOnClick: true,
+      hideOnSwipe: true,
+      resetScroll: true,
+      resetForms: true,
+      side: "left",
+      target: $body,
+      visibleClass: "navPanel-visible",
+    });
+})(jQuery);
+
+// TIME.
+function makeTimer() {
+  //		var endTime = new Date("29 April 2018 9:56:00 GMT+04:00");
+  var endTime = new Date("29 August 2021 8:00:00 GMT+04:00");
+  endTime = Date.parse(endTime) / 1000;
+
+  var now = new Date();
+  now = Date.parse(now) / 1000;
+
+  var timeLeft = endTime - now;
+
+  var days = Math.floor(timeLeft / 86400);
+  var hours = Math.floor((timeLeft - days * 86400) / 3600);
+  var minutes = Math.floor((timeLeft - days * 86400 - hours * 3600) / 60);
+  var seconds = Math.floor(timeLeft - days * 86400 - hours * 3600 - minutes * 60);
+
+  if (hours < "10") {
+    hours = "0" + hours;
+  }
+  if (minutes < "10") {
+    minutes = "0" + minutes;
+  }
+  if (seconds < "10") {
+    seconds = "0" + seconds;
+  }
+
+  // display
+  document.getElementById("timer").innerHTML =
+    '<div class="days"> \
+ <div class="numbers">' +
+    days +
+    '</div>days</div> \
+<div class="hours"> \
+ <div class="numbers">' +
+    hours +
+    '</div>hours</div> \
+<div class="minutes"> \
+ <div class="numbers">' +
+    minutes +
+    '</div>minutes</div> \
+<div class="seconds"> \
+ <div class="numbers">' +
+    seconds +
+    "</div>seconds</div> \
+</div>";
+}
+
+setInterval(function () {
+  makeTimer();
+}, 1000);
